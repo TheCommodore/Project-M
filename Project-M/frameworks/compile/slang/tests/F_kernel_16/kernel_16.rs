55110634c5f58262eae15ed222f54dad671d5f00
Kyler Jeffrey <mbqsniper@hotmail.com>
2013-04-03 11:13:58 -0700

    Bye

--- Project-M/frameworks/compile/slang/tests/F_kernel_16/kernel_16.rs
+++
@@ -1,7 +0,0 @@
-// -target-api 16
-#pragma version(1)
-#pragma rs java_package_name(foo)
-
-void __attribute__((kernel)) root(int i) {
-}
-
--- Project-M/frameworks/compile/slang/tests/F_kernel_ptr_ret_val/kernel_ptr_ret_val.rs
+++
@@ -1,7 +0,0 @@
-#pragma version(1)
-#pragma rs java_package_name(foo)
-
-int * __attribute__((kernel)) root() {
-  return NULL;
-}
-
